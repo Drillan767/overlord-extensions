04236836ec7e8fb076b66d8ee871ce2de2f33e22
Joseph Levarato <joseph@idusante.fr>
2023-01-29 16:51:04 +0100

    Fixed + cleaned hook post process

--- hooks/postprocess/src/index.ts
+++ hooks/postprocess/src/index.ts
@@ -1,33 +1,27 @@
 import { defineHook } from '@directus/extensions-sdk';
 
 type Content = {
-    title: string,
-	body: string,
-	toc: string,
-	slug: string
+    title?: string
+    slug: string
 }
 
-type Header = {
-	id: string,
-	title: string
-	level: Number,
-}
+const events = ['items.create', 'items.update']
 
 export default defineHook(({ filter }) => {
-    ['Articles', 'Project', 'Pages'].forEach((type) => {
-        filter(`${type}.items.create`, async (item) => handle(item))
-        filter(`${type}.items.update`, async (item) => handle(item))
+    filter('*.*', (payload, meta) => {
+        if (events.includes(meta.event) && /_translations$/.test(meta.collection)) {
+            const item = payload as Content
+
+            if (item.title) {
+                item.slug = slugify(item.title)
+            }
+
+            return item
+        }
+
+        return payload
     })
 })
-
-const handle = (item: unknown) => {
-    const element = item as Content
-    if (element.hasOwnProperty('title')) {
-        element.slug = slugify(element.title)
-    }
-
-    return element
-}
 
 const slugify = (string: string) => {
 	return string
